7e255632621d3e7c22ae00fcfce5611ba4ce6e9c
Z@idi#12 <syedafarha.zaidi@gmail.com>
2024-08-03 15:57:50 +0530

    cloud function scripts and deploy files

---
+++ cloud_function1/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Ensure the script stops if any command fails
+set -e
+
+# Set environment variables if needed
+export GOOGLE_APPLICATION_CREDENTIALS="C:\Users\GAPL_\Downloads\devops-testing-419206-abe64bfec3fe.json"
+
+# Set variables for deployment
+FUNCTION_NAME="cloud_function1"
+ENTRY_POINT="main"  # The function name to be used as the entry point in your Python script
+REGION="asia-south1 (Mumbai)"  # e.g., us-central1
+PROJECT_ID="devops-testing-419206"
+RUNTIME="python39"  # Python runtime version
+
+# Deploy the function
+gcloud functions deploy $FUNCTION_NAME \
+    --entry-point $ENTRY_POINT \
+    --runtime $RUNTIME \
+    --trigger-http \
+    --allow-unauthenticated \
+    --region $REGION \
+    --project $PROJECT_ID
+
+# Check deployment status
+if [ $? -eq 0 ]; then
+    echo "Deployment successful for function: $FUNCTION_NAME"
+else
+    echo "Deployment failed for function: $FUNCTION_NAME"
+    exit 1
+fi
---
+++ cloud_function2/main.py
@@ -0,0 +1,18 @@
+def main(request):
+    """
+    HTTP Cloud Function.
+    
+    Args:
+        request (google.cloud.functions_v1.types.HttpRequest): The HTTP request object.
+    
+    Returns:
+        A plain text response indicating the success of the function.
+    """
+    # Check for a "status" query parameter
+    status = request.args.get('status', 'unknown')
+    
+    # Create a response message
+    response_message = f"Function received status: {status}"
+    
+    # Return the response as plain text
+    return response_message
